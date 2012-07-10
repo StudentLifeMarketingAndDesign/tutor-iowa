3e6ab4786aad6ad24b9bf231a99d7bbf26a1a966
Parker <amparke@dhcp80ff31a8.dynamic.uiowa.edu>
2012-07-10 09:53:42 -0500

    EmailArray updated query for new role

--- mysite/code/EmailArray.php
+++ mysite/code/EmailArray.php
@@ -1,4 +1,7 @@
 <?php
 //Change title at far-right of statement to change group that gets notifications of users registering
-$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select ID from `Group` where title='Content Authors'))");
+
+//$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select ID from `Group` where title='Content Authors'))");
+
+$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select GroupID from Group_Roles where PermissionRoleID = (select ID from `PermissionRole` where title='GetsUserEmails')))");
 ?>
