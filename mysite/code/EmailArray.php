1e8ffe45d4a7a03352ef63184f6ae5e171ab25bd
Parker <amparke@dhcp80ff31a8.dynamic.uiowa.edu>
2012-07-17 09:17:28 -0500

    Changed email array to get all content authors instead of just a specific role

--- mysite/code/EmailArray.php
+++ mysite/code/EmailArray.php
@@ -1,9 +1,14 @@
 <?php
 //Change title at far-right of statement to change group that gets notifications of users registering
 
+//old queries: for reference purposes
+
 //$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select ID from `Group` where title='Content Authors'))");
+
+//$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select GroupID from Group_Roles where PermissionRoleID = (select ID from `PermissionRole` where title='GetsUserEmails')))");
 
 //select * from member where ID in (select MemberID from Group_Members where GroupID = (select GroupID from Group_Roles where PermissionRoleID = (select ID from `PermissionRole` where title='GetsUserEmails')))
 
-$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select GroupID from Group_Roles where PermissionRoleID = (select ID from `PermissionRole` where title='GetsUserEmails')))");
+$emailArray = DataObject::get('Member', "ID in (select MemberID from Group_Members where GroupID = (select ID from `Group` where title='Content Authors'))");
+
 ?>
