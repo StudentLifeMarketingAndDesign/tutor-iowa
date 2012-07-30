78b1b942186eac2ed98d609535fea14dfb2592f6
Parker <amparke@dhcp80ff31a8.dynamic.uiowa.edu>
2012-07-30 14:12:43 -0500

    Cleaned up code some and added some comments -- pretty much entirely cosmetic changes

--- mysite/code/MemberDecorator.php
+++ mysite/code/MemberDecorator.php
@@ -16,60 +16,11 @@ class MemberDecorator extends DataObjectDecorator {
              ),
         );
     }
-    
- 
-     /*
-    //Add form fields to CMS
-    public function updateCMSFields(FieldSet &$fields) 
-    {
-             
-        $fields->addFieldToTab("Root.Profile", new TextareaField('Bio', 'Bio')); 
-        $fields->addFieldToTab("Root.Profile", new TextField('PhoneNo', 'Phone No'));
-       
-       $tagsTablefield = new DataObjectManager(
-         $this,
-         'Tags',
-         'Tag',
-         array(
-        'TagLabel' => 'Tag Label'
-         ),
-         'getCMSFields_forPopup'
-      );
-      
-      //$tablefield->setParentClass('Member');
-      
-       $fields->addFieldToTab( 'Root.Tags', $tagsTablefield );
-       
-       /*
-       $tagsTable = new ComplexTableField($this, 'Tags', 'Tag');
-      $fields->addFieldToTab( 'Root.Tags', $tagsTable);
-      
-       return $fields;
 
-          
-    }
-    
-    public function getTags(){
-	    //return this->extraStatics->Tags();
-    }
-    
-    function populateTemplate() {
-	 if (isset($_GET['ID'])){
-	 	$ID = $_GET['ID'];
-	 	if (is_int(intval($ID))){
-	  		$templateInfo = DataObject::get_by_id("SupplementalInstruction", $ID);
-	  		return $templateInfo;
-	  	}
-	 }
-  }
-*/  
 }
-
-
 
 class MemberDecorator_Controller extends Page_Controller { 
 		
     
 } 
 
-*/
