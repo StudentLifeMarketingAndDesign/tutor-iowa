3ab20d65e110085540fb72b018723580d9d753fe
Parker <amparke@dhcp80ff31a8.dynamic.uiowa.edu>
2012-08-01 14:26:03 -0500

    need to pull

---
+++ mysite/code/StatsPage.php
@@ -0,0 +1,23 @@
+<?php
+class StatsPage extends Page {
+
+ 
+    
+    
+   public function getCMSFields() {
+        $fields = parent::getCMSFields();
+     
+        $fields->addFieldToTab('Root.Content.Main', $tutorCount = new TextField('Placeholder1', 'Number of tutors in database'));
+        $fields->addFieldToTab('Root.Content.Main', $tutorRequestCount = new TextField('Placeholder2', 'Number of tutor requests made'));
+              
+        $set = DataObject::get("TutorPage");
+        $count = $set->Count();
+        $tutorCount->setValue($count);
+        
+        
+               
+        return $fields;
+    }
+}
+class StatsPage_Controller extends Page_Controller {
+}
