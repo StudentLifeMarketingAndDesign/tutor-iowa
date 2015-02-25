3362ecdbf45a4c74d65fdab9353dd4b725aee313
Dustin Quam <dustinquam@me.com>
2015-02-25 16:38:16 -0600

    Basic popular search term functionality

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -6,9 +6,9 @@ class Page extends SiteTree {
 	private static $has_one = array(
 		"Image" => "Image",
 		"BackgroundImage" => "Image",
-		);
+	);
 
-	private static $defaults = array('ProvideComments' => '1', );
+	private static $defaults = array('ProvideComments' => '1');
 
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
@@ -19,6 +19,23 @@ class Page extends SiteTree {
 }
 
 class Page_Controller extends ContentController {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array('logout');
 
 	public function SplitKeywords() {
 		$keywords = $this->MetaKeywords;
@@ -55,8 +72,6 @@ class Page_Controller extends ContentController {
 
 	public function getHelpLab() {
 		if ($this->isHelpLab()) {
-
-			//$HelpLab = DataObject::get_one("YourHelpLabs");
 			$HelpLab = YourHelpLabs::get("YourHelpLabs")->First();
 			return $HelpLab;
 		}
@@ -74,8 +89,6 @@ class Page_Controller extends ContentController {
 
 		$form = new Form($this, 'NewsletterSignUpForm', $fields, $actions, $validator);
 		$form->enableSpamProtection();
-
-		//$protector = SpamProtectorManager::update_form($form, 'Message');
 
 		return $form;
 	}
@@ -96,9 +109,6 @@ class Page_Controller extends ContentController {
 		if ($Member) {
 
 			$IDMember = $Member->ID;
-			//$memberLabs = DataObject::get('Member', "ID in (SELECT DISTINCT MemberID from  `HelpLab_Members`)");
-
-			//$memberLabs = DataObject::get('Member', "ID=$IDMember and ID in (SELECT DISTINCT MemberID from  `HelpLab_Members`)");
 			$memberLabs = Member::get()->where("ID=$IDMember and ID in (SELECT DISTINCT MemberID from  `HelpLab_Members`)");
 
 			return $memberLabs;
@@ -107,21 +117,8 @@ class Page_Controller extends ContentController {
 		return false;
 	}
 
-	/*
-	public function isHelpLabCached(){
-	return (Session::get("isHelpLabCached") == 1);
-	}
-
-	public function isHelpLabSession(){
-	return (Session::get("isHelpLabSession") == 1);
-	}
-	 */
-
 	public function News($number = 3) {
-
-		//$articles = DataObject::get("ArticlePage", $filter = null, $sort = "Date DESC", $join = null, $limit = $number);
 		$articles = ArticlePage::get()->sort('Date DESC');
-
 		if ($articles) {
 			return $articles;
 		}
@@ -147,14 +144,13 @@ class Page_Controller extends ContentController {
 		$keyword = Convert::raw2sql($keyword);
 		$keywordHTML = htmlentities($keyword, ENT_NOQUOTES, 'UTF-8');
 
+		$this->addSearchTermToLibrary($keyword);
+
 		$pages = new ArrayList();
 		$news = new ArrayList();
 		$files = new ArrayList();
 
 		$mode = ' IN BOOLEAN MODE';
-
-		//$mode = ' WITH QUERY EXPANSION';
-		//$mode = '';
 
 		$siteTreeClasses = array('TutorPage', 'SupplementalInstruction', 'HelpLab');
 		//add in an classes that extend Page or SiteTree
@@ -181,21 +177,9 @@ class Page_Controller extends ContentController {
 			$pages->merge($objects);
 		}
 
-		/* ** */
 		$pages->sort(array('Relevance' => 'DESC', 'Title' => 'ASC'));
 
 		$data = array('Tutors' => $pages->filter(array('ClassName' => 'TutorPage')), 'SupplementalInstructions' => $pages->filter(array('ClassName' => 'SupplementalInstruction')), 'HelpLabs' => $pages->filter(array('ClassName' => 'HelpLab')), 'Query' => $keyword, 'Title' => 'Search Results');
-
-		/*
-
-		$data = array(
-		'Results' => $results,
-		'Tutors' => $tutors,
-		'SupplementalInstructions' => $supplementalInstructions,
-		'HelpLabs' => $helpLabs,
-		'Query' => $form->getSearchQuery(),
-		'Title' => 'Search Results'
-		);*/
 
 		if ($pages->count() == 0 && $news->count() == 0 && $files->count() == 0) {
 			$data['NoResults'] = 1;
@@ -205,66 +189,20 @@ class Page_Controller extends ContentController {
 		return $this->customise($data)->renderWith(array('Page_results', 'Page'));
 	}
 
-	/* Old Results Function For Reference Only */
+	private function addSearchTermToLibrary($keyword) {
 
-	/*
-	function results($data, $form){
-	$form->setPageLength(99999); //Makeshift way of disabling pagination for results
-	$results = $form->getResults();
+		$term = SearchTerm::get()->filter(array('Title' => $keyword))->First();
 
-
-	$supplementalInstructions = new ArrayList();
-	$tutors = new ArrayList();
-	$helpLabs = new ArrayList();
-
-	foreach($results AS $result) {
-
-	if($result->ClassName == "SupplementalInstruction") {
-	$supplementalInstructions->push($result);
-	}
-
-	if($result->ClassName == "TutorPage") {
-
-	//$tutorObject = DataObject::get_by_id("TutorPage", $result->ID);
-	$tutorObject = TutorPage::get()->byID($result->ID);
-	$tutors->push($tutorObject);
-	//print_r($result);
-	}
-
-	if($result->ClassName == "HelpLab") {
-	$helpLabs->push($result);
-	}
+		if (isset($term)) {
+			$term->SearchCount = $term->SearchCount + 1;
+			$term->write();
+		} else {
+			$term = new SearchTerm();
+			$term->Title = $keyword;
+			$term->write();
+		}
 
 	}
-	//print_r($tutors);
-	$data = array(
-	'Results' => $results,
-	'Tutors' => $tutors,
-	'SupplementalInstructions' => $supplementalInstructions,
-	'HelpLabs' => $helpLabs,
-	'Query' => $form->getSearchQuery(),
-	'Title' => 'Search Results'
-	);
-
-	return $this->customise($data)->renderWith(array('Page_results', 'Page'));
-	}*/
-
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array('logout');
 
 	//I want logout to redirect to the home page
 
@@ -284,36 +222,9 @@ class Page_Controller extends ContentController {
 	}
 
 	public function LatestNews($num = 5) {
-		//$news = DataObject::get_one("ArticleHolder");
 		$news = ArticlePage::get()->sort('Sort')->limit($num);
-		//return ($news) ? DataObject::get("ArticlePage", "ParentID = $news->ID", "Date DESC", "", $num) : false;
 		return $news;
 
-	}
-
-	public function init() {
-		parent::init();
-
-		//require_once '/Applications/MAMP/htdocs/tutoriowa3/ChromePhp.php';
-		Requirements::block('/cms/css/layout.css');
-
-		if (isset($_GET['setTheme'])) {
-			if (Director::isDev() || Permission::check('ADMIN')) {
-				Session::set('theme', $_GET['setTheme']);
-			} else {
-				Security::permissionFailure(null, 'Please log in as an administrator to switch theme.');
-			}
-		}
-
-		if (isset($_GET['theme'])) {
-			if (Director::isDev() || Permission::check('ADMIN')) {
-				SSViewer::set_theme($_GET['theme']);
-			} else {
-				Security::permissionFailure(null, 'Please log in as an administrator to set the theme.');
-			}
-		} elseif (Session::get('theme')) {
-			SSViewer::set_theme(Session::get('theme'));
-		}
 	}
 
 	function getHelpLabs() {
