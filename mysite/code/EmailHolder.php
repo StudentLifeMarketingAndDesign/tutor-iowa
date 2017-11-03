f405bae161eb4473e13a9cbf56bf4c65b10cbeef
Dustin Quam <quamsta@gmail.com>
2017-11-03 16:49:13 -0500

    updates

--- mysite/code/EmailHolder.php
+++ mysite/code/EmailHolder.php
@@ -25,7 +25,7 @@ class EmailHolder extends Page {
     	$fields->removeFieldFromTab('Root.Metadata', "Keywords"); 
         $fields->removeFieldFromTab('Root.Main', "Content");
         $fields->addFieldToTab( 'Root.Main', new HTMLEditorField("RegistrationRequest", "Edit the registration request here"));
-        $fields->addFieldToTab( 'Root.CMain', new HTMLEditorField("RegistrationConfirm", "Edit the registration confirmation email here"));
+        $fields->addFieldToTab( 'Root.Main', new HTMLEditorField("RegistrationConfirm", "Edit the registration confirmation email here"));
        // $fields->addFieldToTab( 'Root.Content.Main', new HTMLEditorField("RequestTutor", "Edit the email that goes out to request a tutor here"));
         $fields->addFieldToTab( 'Root.Main', new HTMLEditorField("DisablePage", "Edit the email a user gets when they request to disable their page here"));
         $fields->addFieldToTab( 'Root.Main', new HTMLEditorField("EnablePage", "Edit the email a user gets when they request to enable their page here"));
