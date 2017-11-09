2d1cb997dabe8cb45b12d32758dd957547e4b944
Dustin Quam <quamsta@gmail.com>
2017-11-09 16:22:05 -0600

    removed email upon unpublish

--- mysite/code/extensions/TutorPageExtension.php
+++ mysite/code/extensions/TutorPageExtension.php
@@ -1,22 +1,6 @@
 <?php
 class TutorPageExtension extends DataExtension {
 
-	public function onAfterUnpublish() {
-
-		$adminEmail = Config::inst()->get('Email', 'admin_email');
-		$subject = "Your Tutor Iowa page has been disabled";
-		$body = "You can request your details be edited <a href='" . Director::absoluteBaseURL() . "edit-profile-page'>here</a/> ";
-
-		$email = new Email();
-		$email->setTo($this->owner->Bio);
-		$email->setFrom($adminEmail);
-		$email->setSubject($subject);
-		$email->setBody($body);
-		if (SS_ENVIRONMENT_TYPE == "live") {
-			$email->send();
-		}
-
-	}
 
 	public function onBeforeWrite() {
 		if (!$this->owner->EligibleToTutor) {
